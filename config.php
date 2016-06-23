283aa6b11afce04885abb4d41a325386168b9375
Steven Harradine <stevenharradine@gmail.com>
2016-06-23 11:12:00 -0400

    update config options

--- config.php
+++ config.php
@@ -1,5 +1,10 @@
 <?php
-	$username = '';
-	$password = '';
-	$reportSuiteID = '';
-	$cache " '';
+        $username = '';
+        $password = '';
+        $reportSuiteID = '';
+        $useCache = false;
+        $alert_threshold = 100;
+        $pagerduty_api_accesskey = '';
+        $pagerduty_servicekey = '';
+        $cache_incidents = '';
+        $cache_realtime_normal = '';
